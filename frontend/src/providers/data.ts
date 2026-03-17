9fd1bb763fa78b7c68c9bae20364095d1c864a59
Deshan <deshanekanayake2003@gmail.com>
2026-03-17 23:15:04 +0000

    implement coderabbit fixes

--- frontend/src/providers/data.ts
+++ frontend/src/providers/data.ts
@@ -97,7 +97,7 @@ const options: CreateDataProviderOptions = {
     mapResponse: async (response) => {
       const json: GetOneResponse = await response.json();
 
-      return json.data ?? [];
+      return json.data ?? null;
     }
   }
 
